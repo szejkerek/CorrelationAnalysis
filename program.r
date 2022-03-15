224b23bf73b51628de45c99e5205f257c4917953
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2022-03-15 13:01:27 +0100

    Added histogram

--- program.r
+++ program.r
@@ -1,6 +1,13 @@
+
 #WCZYTYWANIE DANYCH
 dane <- read.delim("dane.txt")
 wzrost <- dane[ , c("wzrost")]  
 plec <- dane[ , c("plec")]  
 waga <- dane[ , c("waga")]  
+
+#WZROST
+mean(wzrost)
+sd(wzrost)
+
+hist(wzrost)
 
