e55ac80d526efe09c11f71baa3de85273968a281
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2022-03-19 14:35:40 +0100

    Zadanie 2

--- program.r
+++ program.r
@@ -5,9 +5,26 @@ wzrost <- dane[ , c("wzrost")]
 plec <- dane[ , c("plec")]  
 waga <- dane[ , c("waga")]  
 
-#WZROST
+
+#ZADANIE 1
 mean(wzrost)
 sd(wzrost)
-
+quantile(wzrost)
 hist(wzrost)
+hist(waga)
 
+#ZADANIE 2
+chlopcy <- subset(dane,plec==1)
+dziewczyny <- subset(dane,plec==0)
+
+reglinp = function(X, name){
+  fit <- lm(X$wzrost ~ X$waga)
+  summary(fit)
+  
+  plot(X$waga, X$wzrost, main = name, xlab = "Waga, kg", ylab = "Wzrost, cm") 
+  abline(fit)
+}
+
+reglinp(chlopcy, "chlopcy")
+reglinp(dziewczyny, "dziewczyny")
+reglinp(dane, "chlopcy + dziewczyny")
