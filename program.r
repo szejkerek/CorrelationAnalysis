06397ecd2afd7298384029b4e89c611c83cd7a98
KomorXD <pawel.kupczak.sc@gmail.com>
2022-04-23 16:32:13 +0200

    niech jedna osoba pullnie i zobaczy czy dziala bo moze cos rozjebalem nwmnwm

--- program.r
+++ program.r
@@ -1,94 +1,91 @@
-#WCZYTYWANIE DANYCH
-dane <- read.delim("dane.txt")
-wzrost <- dane[ , c("wzrost")]  
-plec <- dane[ , c("plec")]  
-waga <- dane[ , c("waga")]  
+#  WCZYTYWANIE BIBLIOTEK
+library("MASS")
+library("ggplot2")
+
+# WCZYTYWANIE DANYCH
+data <- read.delim("data.txt")
+
+height_data <- data[ , c("wzrost")]  
+gender_data <- data[ , c("plec")]  
+weight_data <- data[ , c("waga")]  
 
 
-#ZADANIE 1
-mean(wzrost)
-sd(wzrost)
-quantile(wzrost)
-hist(wzrost)
-hist(waga)
+# ZADANIE 1
+mean(height_data)
+sd(height_data)
+quantile(height_data)
+hist(height_data)
+hist(weight_data)
 
-#ZADANIE 2
-chlopcy <- subset(dane,plec==1)
-dziewczyny <- subset(dane,plec==0)
+# ZADANIE 2
+boys  <- subset(data, plec==1)
+girls <- subset(data, plec==0)
 
-modelReglinp <- function(X){
-  return(lm(X$wzrost ~ X$waga))
+model_reglinp <- function(X) {
+  return( lm(X$wzrost ~ X$waga) )
 }
 
 reglinp = function(X, name){
-  fit <- modelReglinp(X)
+  fit <- model_reglinp(X)
+
   summary(fit)
-  
   plot(X$waga, X$wzrost, main = name, xlab = "Waga, kg", ylab = "Wzrost, cm") 
   abline(fit)
+
   return(fit)
 }
 
-reg_chlopcy <- reglinp(chlopcy, "chlopcy")
-reg_dziewczyny <- reglinp(dziewczyny, "dziewczyny")
-reg_wszyscy <- reglinp(dane, "chlopcy + dziewczyny")
+boys_model     <- reglinp(chlopcy,    "chlopcy")
+girls_model    <- reglinp(dziewczyny, "dziewczyny")
+everyone_model <- reglinp(data,       "chlopcy + dziewczyny")
 
-#Zadanie 3
-library("MASS")
-fitdist_chlopcy<- MASS::fitdistr(reg_chlopcy$coefficients, "normal")
-fitdist_dziewczyny<- MASS::fitdistr(reg_dziewczyny$coefficients, "normal")
-fitdist_wszyscy<- MASS::fitdistr(reg_wszyscy$coefficients, "normal")
-fitdist_chlopcy
-fitdist_dziewczyny
-fitdist_wszyscy
-xdd <- reg_chlopcy[["coefficients"]][["(Intercept)"]]
-xdd
-wektor_wsp_kier <- c(reg_chlopcy[["coefficients"]][["(Intercept)"]],
-               reg_dziewczyny[["coefficients"]][["(Intercept)"]],
-               reg_wszyscy[["coefficients"]][["(Intercept)"]])
-wektor_wsp_kier
-wektor_wyr_wolny <-  c(reg_chlopcy[["coefficients"]][["X$waga"]],
-                       reg_dziewczyny[["coefficients"]][["X$waga"]],
-                       reg_wszyscy[["coefficients"]][["X$waga"]])
-wektor_wyr_wolny
-bl_standardowy <- function(x) sd(x)/sqrt(length(x))
-bl_standardowy(wektor_wsp_kier)
-bl_standardowy(wektor_wyr_wolny)
-#błędy są na tyle małe, że 
+# Zadanie 3
+slope_coefficients <- c(reg_chlopcy[["coefficients"]][["(Intercept)"]],
+                        reg_dziewczyny[["coefficients"]][["(Intercept)"]],
+                        reg_wszyscy[["coefficients"]][["(Intercept)"]])
+slope_coefficients
 
-#Zadanie 4
+const_term_coefficients <-  c(reg_chlopcy[["coefficients"]][["X$waga"]],
+                              reg_dziewczyny[["coefficients"]][["X$waga"]],
+                              reg_wszyscy[["coefficients"]][["X$waga"]])
+const_term_coefficients
 
-prediction <- function(dane){
-  
-  model <- modelReglinp(dane)
+std_err <- function(x) sd(x) / sqrt(length(x))
+
+std_err(slope_coefficients)
+std_err(const_term_coefficients)
+
+# Zadanie 4
+prediction <- function(data) {
+  model <- model_reglinp(data)
 
   pred.int <- predict(model, interval = "prediction")
-  mydata <- cbind(dane, pred.int)
+  mydata   <- cbind(data, pred.int)
 
-  library("ggplot2")
-  p <- ggplot(mydata, aes(dane$waga, dane$wzrost)) +
-    geom_point() +
-    stat_smooth(method = lm)
+  p <- ggplot(mydata, aes(data$waga, data$wzrost)) +
+       geom_point() +
+       stat_smooth(method = lm)
 
-  p + geom_line(aes(y = lwr), color = "red", linetype = "dashed")+
-    geom_line(aes(y = upr), color = "red", linetype = "dashed")
-  
+  p + geom_line(aes(y = lwr), color = "red", linetype = "dashed") +
+      geom_line(aes(y = upr), color = "red", linetype = "dashed")
 }
 
 
-prediction(chlopcy)
-prediction(dziewczyny)
-prediction(dane)
+prediction(boys)
+prediction(girls)
+prediction(data)
 
-#Zadanie 5
-resiDUDES <- residuals(reg_chlopcy)
-resiGURLS <- residuals(reg_dziewczyny)
-resiALL <- residuals(reg_wszyscy)
+# Zadanie 5
+resiDUDES <- residuals(boys_model)
+resiGURLS <- residuals(girls_model)
+resiALL   <- residuals(everyone_model)
+
 hist(resiDUDES) 
 hist(resiGURLS)
 hist(resiALL)
-#if p-value > 0.05 then the distribution can be considered as normal
-shapiro.test(resiDUDES) #not-normal distribution
-shapiro.test(resiGURLS) #normal distribution
-shapiro.test(resiALL)   #normal distribution
+
+# jeżeli p-value > 0.05, to rozkład danych możemy uznać za normalny
+shapiro.test(resiDUDES) # rozkład nie jest normalny
+shapiro.test(resiGURLS) # rozkład normalny
+shapiro.test(resiALL)   # rozkład normalny
 
