df80875ceac91641880e39da850351922da085f2
lilboisoul <kdusza2001@gmail.com>
2022-04-23 13:16:53 +0200

    zadanie 3 i 5

--- program.r
+++ program.r
@@ -26,15 +26,37 @@ reglinp = function(X, name){
   
   plot(X$waga, X$wzrost, main = name, xlab = "Waga, kg", ylab = "Wzrost, cm") 
   abline(fit)
-  
+  return(fit)
 }
 
-reglinp(chlopcy, "chlopcy")
-reglinp(dziewczyny, "dziewczyny")
-reglinp(dane, "chlopcy + dziewczyny")
-
+reg_chlopcy <- reglinp(chlopcy, "chlopcy")
+reg_dziewczyny <- reglinp(dziewczyny, "dziewczyny")
+reg_wszyscy <- reglinp(dane, "chlopcy + dziewczyny")
 
 #Zadanie 3
+library("MASS")
+fitdist_chlopcy<- MASS::fitdistr(reg_chlopcy$coefficients, "normal")
+fitdist_dziewczyny<- MASS::fitdistr(reg_dziewczyny$coefficients, "normal")
+fitdist_wszyscy<- MASS::fitdistr(reg_wszyscy$coefficients, "normal")
+fitdist_chlopcy
+fitdist_dziewczyny
+fitdist_wszyscy
+xdd <- reg_chlopcy[["coefficients"]][["(Intercept)"]]
+xdd
+wektor_wsp_kier <- c(reg_chlopcy[["coefficients"]][["(Intercept)"]],
+               reg_dziewczyny[["coefficients"]][["(Intercept)"]],
+               reg_wszyscy[["coefficients"]][["(Intercept)"]])
+wektor_wsp_kier
+wektor_wyr_wolny <-  c(reg_chlopcy[["coefficients"]][["X$waga"]],
+                       reg_dziewczyny[["coefficients"]][["X$waga"]],
+                       reg_wszyscy[["coefficients"]][["X$waga"]])
+wektor_wyr_wolny
+bl_standardowy <- function(x) sd(x)/sqrt(length(x))
+bl_standardowy(wektor_wsp_kier)
+bl_standardowy(wektor_wyr_wolny)
+#błędy są na tyle małe, że 
+
+#Zadanie 4
 
 prediction <- function(dane){
   
@@ -57,4 +79,16 @@ prediction <- function(dane){
 prediction(chlopcy)
 prediction(dziewczyny)
 prediction(dane)
+
+#Zadanie 5
+resiDUDES <- residuals(reg_chlopcy)
+resiGURLS <- residuals(reg_dziewczyny)
+resiALL <- residuals(reg_wszyscy)
+hist(resiDUDES) 
+hist(resiGURLS)
+hist(resiALL)
+#if p-value > 0.05 then the distribution can be considered as normal
+shapiro.test(resiDUDES) #not-normal distribution
+shapiro.test(resiGURLS) #normal distribution
+shapiro.test(resiALL)   #normal distribution
 
